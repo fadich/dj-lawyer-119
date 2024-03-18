78e26f072f5903f72833b3d039fd7f6c7d308b24
Paul8929 <pchepurniak@gmail.com>
2024-03-18 21:15:15 +0200

    added models

--- services/models.py
+++ services/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Service(models.Model):
+    name = models.CharField(max_length=256)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE, related_name='services')
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = (
+            ('name', 'domain'),
+        )
+
+
+# class Lawyer(models.Model):
+#     name = models.CharField(max_length=256)
+#     experience = models.CharField(max_length=256)
+#     description = models.TextField()
+#     location = models.CharField(max_length=256)
+#     specialisation = models.CharField(max_length=256)
+#     services = models.TextField()
+
+
+
